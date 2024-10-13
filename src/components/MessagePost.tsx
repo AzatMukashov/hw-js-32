4411be60d3a981020a4d37611f8f8e4fd8d06aa3
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-10-14 01:43:24 +0600

    создал интерфейс с функцией которая принимает имя автора и его сообщение, в компоненте принял этот интерфейс с пропсом, создал два хука для хранения состояний сообщения и автора, создал функцию отправки содержимого формы с предотвращением релоуда с вызовом функции из интерфейса и очисткой формы после отправки, разметка формы с полями ввода имени автора и сообщения также кнопка отправки содержимого полей автора и сообщения

--- src/components/MessagePost.tsx
+++ src/components/MessagePost.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+interface MessagePostProps {
+  onSendMessage: (message: string, author: string) => void;
+}
 
-
-const MessagePost = () => {
+const MessagePost: React.FC<MessagePostProps> = ({onSendMessage}) => {
+  const [message, setMessage] = useState('');
+  const [author, setAuthor] = useState('');
+  const handleSendMessage = (e: React.FormEvent) => {
+    e.preventDefault();
+    onSendMessage(message, author);
+    setMessage('');
+  }
   return (
-    <div>
-      
-    </div>
+    <form onSubmit={handleSendMessage}>
+      <input type="text"
+             value={author}
+             onChange={(e) => setAuthor(e.target.value)}
+             required
+             placeholder="Name"/>
+      <input type="text"
+             value={message}
+             onChange={(e) => setMessage(e.target.value)}
+             required
+             placeholder="Message"/>
+      <button type="submit">Send</button>
+    </form>
   );
 };
 
