80b4a42f70bc23078b1a9966a564396a1d1f33fc
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-10-14 16:26:16 +0600

    Создал интерфейс, где дал тип массиву. В самом компоненте принял интерфейс с пропсом. Создал разметку с выводом данных через цикл

--- src/components/ViewMessages.tsx
+++ src/components/ViewMessages.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Massage } from '../types';
 
-const ViewMessages = () => {
+interface ViewMessageProps {
+  messages: Massage[];
+}
+
+const ViewMessages: React.FC<ViewMessageProps> = ({messages}) => {
   return (
     <div>
-
+      {messages.map((msg) => (
+        <div key={msg._id}>
+          <p><strong>{msg.author}</strong>: {msg.message}</p>
+          <p><small>{new Date(msg.datetime).toLocaleString()}</small></p>
+        </div>
+      ))}
     </div>
   );
 };
